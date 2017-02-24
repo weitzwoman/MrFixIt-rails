a740d6aa24445e72ab1991030d6fac5e2d677c97
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-23 19:41:03 -0800

    add spec models and pass validation specs for job, add database relationship that might change

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ActiveRecord::Base
-  validates :title, :presence => true
+  validates :title, :description, :presence => true
+  belongs_to :worker
 end
---
+++ spec/models/job_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Job do
+  it { should belong_to :worker }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :description }
+end
