121274743dc29c4ebfd0d4c562ef1a014433c658
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-24 09:40:47 -0800

    add passing spec for error creating job

--- spec/features/add_jobs_spec.rb
+++ spec/features/add_jobs_spec.rb
@@ -15,4 +15,16 @@ describe "how user creates job" do
     expect(page).to have_content("Toliet")
   end
 
+  it "show errors for creating job when field is empty" do
+    user = create(:user)
+    visit "/"
+    click_on "Log In as User"
+    fill_in "Email", :with => "user@email.com"
+    fill_in "Password", :with => "password"
+    click_on "Log in"
+    click_on "Add Your Job"
+    click_on "Create Job"
+    expect(page).to have_content('Job Not Created')
+  end
+
 end
