07b0b607e99dd4f359f01478b3978c1254a2cc74
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-24 16:54:50 -0800

    still can't get ajax buttons to work, this project was really frustrating

---
+++ app/assets/javascripts/jobs.js
@@ -0,0 +1,6 @@
+// $(function() {
+//   $('.edit_job input[type=submit]').remove();
+//   $('.edit_job input[type=checkbox]').click(function() {
+//     $(this).parent('form').submit();
+//   });
+// });
